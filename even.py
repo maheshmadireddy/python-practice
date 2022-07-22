8e6cd0087ee31248a04e26cf359eb7890cbf7cf2
maheshmadireddy <103639468+maheshmadireddy@users.noreply.github.com>
2022-07-22 18:05:08 +0530

    file commits

---
+++ even.py
@@ -0,0 +1,14 @@
+#even numbers programm
+i=int(input('Enter the value of i : '))
+if(i%2==0):
+    print('The given number is even number')
+else:
+    print('The number is odd number')
+
+
+    
+
+
+
+
+
